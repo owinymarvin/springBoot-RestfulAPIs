f7af5c59281e351d3b3dbd205e9a8b3ae2fcd898
timothy <timothy@dmarv.dmarv.com>
2025-06-28 10:10:39 +0300

    Added an Order And Order Mapping. Record Vs POJO

--- src/main/java/com/dmarv/restfulAPIs/FirstController.java
+++ src/main/java/com/dmarv/restfulAPIs/FirstController.java
@@ -21,6 +21,23 @@ public class FirstController {
         return "Request Accepted and Order is:  " + order.toString();
     }
 
-    @PostMapping("/api/record")
-    public String record(){return "";}
+    @PostMapping("/api/post/record")
+    public String postRecord(@RequestBody OrderRecord order){
+        return "Request Accepted and the Order is: " + order.toString();
+    }
+
+//    getParams from the URL
+    @GetMapping("/api/hello/{userName}")
+    public String pathVariable(@PathVariable("userName") String userName){
+        return "My URL variable: " + userName;
+    }
+//    get multiple Params from the url as shown below
+//    http://127.0.0.1:8080/api/multiple/variables?username=owinymarvin&lastname=timothy
+    @GetMapping("/api/multiple/variables")
+    public String multipleParams(
+            @RequestParam("username") String userName,
+            @RequestParam("lastname") String userLastName
+    ){
+        return "My values: username: " + userName + "  lastname: " + userLastName;
+    }
 }
